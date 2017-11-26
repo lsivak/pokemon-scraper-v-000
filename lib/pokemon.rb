0fac364060d5f466e5f9c8b2bfb9c27fde70f6de
Lara Sanders <larasanders@gmail.com>
2017-11-26 20:46:09 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,8 +26,7 @@ Pokemon.new(@id, "Pikachu", "electric", @db)
 
 def self.find(id, db)
   where_pokemon = db.execute("SELECT id FROM pokemon WHERE id = ?", id)
-  Pokemon.new(where_pokemon[0], where_pokemon[1], where_pokemon[2], db)
-  id = where_pokemon[0]
+where_pokemon
 binding.pry
 end
 
