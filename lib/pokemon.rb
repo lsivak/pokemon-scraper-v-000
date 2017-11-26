2d8d0947c9c05bf31bbb203fff080e5c15d111d6
Lara Sanders <larasanders@gmail.com>
2017-11-26 21:29:26 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,7 +7,7 @@ class Pokemon
     @id = id
     @name = name
     @type = type
-    @db = SQLite3::Database.new('db/pokemon.db')
+    @db = db
 
 end
 
@@ -16,7 +16,7 @@ def self.save(name, type, db)
   db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)", name, type)
 end
 
-
+@db = SQLite3::Database.new('db/pokemon.db')
 self.new("Pikachu", "electric", @db)
 
 def self.find(id, db)
